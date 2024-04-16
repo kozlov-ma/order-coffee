c8529ac5377efa3f3013c81c8a3c5aba8494f5c3
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-16 18:50:58 +0500

    данные из форм

--- index.js
+++ index.js
@@ -29,34 +29,64 @@ function newBeverageForm() {
 
 (function () {
     customElements.define('beverage-component', class extends HTMLElement {
-        constructor() {
-            super();
-            const template = document.getElementById('beverage-template').content;
-            const shadowRoot = this.attachShadow({mode: 'open'});
-            shadowRoot.appendChild(template.cloneNode(true));
+            constructor() {
+                super();
+                const template = document.getElementById('beverage-template').content;
+                const shadowRoot = this.attachShadow({mode: 'open'});
+                shadowRoot.appendChild(template.cloneNode(true));
 
-            const beverageCount = shadowRoot.querySelector('.beverage-count');
-            beverageCount.textContent = `Напиток №${this.getAttribute('beverage-number') || ''}`;
+                const beverageCount = shadowRoot.querySelector('.beverage-count');
+                beverageCount.textContent = `Напиток №${this.getAttribute('beverage-number') || ''}`;
 
-            const removeButton = shadowRoot.querySelector('.remove-button');
-            removeButton.addEventListener('click', () => {
-                if (count <= 1)
-                    return;
+                const removeButton = shadowRoot.querySelector('.remove-button');
+                removeButton.addEventListener('click', () => {
+                    if (count <= 1)
+                        return;
 
-                count--;
-                this.remove();
-            });
-        }
+                    count--;
+                    this.remove();
+                });
 
-        static get observedAttributes() {
-            return ['beverage-number'];
-        }
+                shadowRoot.querySelectorAll('.beverage select, .beverage input, .beverage textarea').forEach(element => {
+                    element.addEventListener('input', () => {
+                        handleChangeSelect(element);
+                    });
+                });
+            }
 
-        attributeChangedCallback(name, oldValue, newValue) {
-            if (name === 'beverage-number') {
-                const beverageCount = this.shadowRoot.querySelector('.beverage-count');
-                beverageCount.textContent = `Напиток №${newValue}`;
+            static
+            get observedAttributes() {
+                return ['beverage-number'];
+            }
+
+            attributeChangedCallback(name, oldValue, newValue) {
+                if (name === 'beverage-number') {
+                    const beverageCount = this.shadowRoot.querySelector('.beverage-count');
+                    beverageCount.textContent = `Напиток №${newValue}`;
+                }
             }
         }
-    });
+    )
+    ;
 })();
+
+function handleRemove(button) {
+    const fieldset = button.closest('.beverage');
+    fieldset.remove();
+}
+
+function handleChangeSelect(select) {
+    const fieldset = select.closest('.beverage');
+    const jsonData = {
+        beverage: select.value,
+        milk: fieldset.querySelector('input[name="milk"]:checked').value,
+        options: Array.from(fieldset.querySelectorAll('input[name="options"]:checked')).map(option => option.value),
+        additional: textValue(fieldset.querySelector('#user-input').textContent)
+    };
+    console.log(jsonData);
+}
+
+function textValue(s) {
+    const keywords = ['срочно', 'быстрее', 'побыстрее', 'скорее', 'поскорее', 'очень нужно'];
+    return s.replace(new RegExp(keywords.join("|"), "g"), match => `<b>${match}</b>`);
+}
